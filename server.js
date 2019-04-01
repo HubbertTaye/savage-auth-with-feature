3cb9ef6eba4b0ff41dfcefceb80351609b600ea2
Taye Hubbert <tayehubbert@gmail.com>
2019-04-01 15:18:27 -0400

    first commit

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // server.js
 
 // set up ======================================================================
-// get all the tools we need
+// get all the tools we need (modules)
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || 8080; //either enviornment variable port or 8080 (will set up enviornment later)
 const MongoClient = require('mongodb').MongoClient
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+var mongoose = require('mongoose'); //works with mongodb
+var passport = require('passport'); //authentication module (create users, log in session, etc)
+var flash    = require('connect-flash'); //view/access error messages. 'wrong password' etc
 
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
+var morgan    = require('morgan'); //debugger, also logs all requests in cmd
+var cookieParser = require('cookie-parser'); //work w/ cookies. in this case user login session
 var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var session      = require('express-session'); //where ever u move in the app, u r still logged in thus can always go back to the profile page. this is called a session (idk if its related to this part)
 
-var configDB = require('./config/database.js');
+var configDB = require('./config/database.js'); //stores databse object contains url and dbName from file config/database.js made for keeping database info secret when pushing to git
 
 var db
 
@@ -23,19 +23,8 @@ var db
 mongoose.connect(configDB.url, (err, database) => {
   if (err) return console.log(err)
   db = database
-  require('./app/routes.js')(app, passport, db);
+  require('./app/routes.js')(app, passport, db); //passes app, passport and db into file routes in folder app. routes.js runs a function
 }); // connect to our database
-
-//app.listen(port, () => {
-    // MongoClient.connect(configDB.url, { useNewUrlParser: true }, (error, client) => {
-    //     if(error) {
-    //         throw error;
-    //     }
-    //     db = client.db(configDB.dbName);
-    //     console.log("Connected to `" + configDB.dbName + "`!");
-    //     require('./app/routes.js')(app, passport, db);
-    // });
-//});
 
 require('./config/passport')(passport); // pass passport for configuration
 
